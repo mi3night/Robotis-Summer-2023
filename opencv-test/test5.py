2798a89a1daf144de44f4bccf6234a48a71d6a49
Newtonnatee <135576562+Newtonnatee@users.noreply.github.com>
2023-06-14 21:01:15 -0700

    AR Marker + Distance

---
+++ opencv-test/test5.py
@@ -0,0 +1,104 @@
+import numpy as np
+import time
+import cv2
+
+
+ARUCO_DICT = {
+    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+    "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+    "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+    "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+    "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+def aruco_display(corners, ids, rejected, image):
+    if len(corners) > 0:
+        ids = ids.flatten()
+        
+        for (markerCorner, markerID) in zip(corners, ids):
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+            cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+            cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+            
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+            
+            cv2.putText(image, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 2)
+            
+            # Calculate distance
+            marker_size = np.linalg.norm(np.array(topRight) - np.array(topLeft))
+            distance = calculate_distance(marker_size)
+            print("[Inference] ArUco marker ID: {}, Distance: {} units".format(markerID, distance))
+            
+    return image
+
+
+def calculate_distance(marker_size):
+    # Constants for your specific camera setup
+    # You need to calibrate these values for your camera
+    marker_size_at_one_meter = 0.3332371  # Adjust this value based on the actual marker size at 1 meter distance
+    focal_length = 100  # Focal length of your camera in pixels
+    
+    # Convert meters to feet
+    distance_in_feet = marker_size_at_one_meter * focal_length / marker_size * 3.28084
+    return distance_in_feet
+
+
+
+
+
+aruco_type = "DICT_5X5_100"
+arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
+arucoParams = cv2.aruco.DetectorParameters_create()
+
+cap = cv2.VideoCapture(1)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+while cap.isOpened():
+    ret, img = cap.read()
+
+    h, w, _ = img.shape
+    width = 1000
+    height = int(width*(h/w))
+    img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+ 
+    corners, ids, rejected = cv2.aruco.detectMarkers(img, arucoDict, parameters=arucoParams)
+
+    detected_markers = aruco_display(corners, ids, rejected, img)
+
+    cv2.imshow("Image", detected_markers)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
