fe1894cd4594a47b1356a72961e1225a5a97833a
decryptec <125177998+decryptec@users.noreply.github.com>
2023-06-18 00:36:05 -0700

    Improve visibility with purple and larger text
    Changed from green to purple and 0.5 to 0.6 text size

--- opencv-test/test5.py
+++ opencv-test/test5.py
@@ -40,10 +40,10 @@ def aruco_display(corners, ids, rejected, image):
             bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
             topLeft = (int(topLeft[0]), int(topLeft[1]))
 
-            cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
-            cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
-            cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
-            cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+            cv2.line(image, topLeft, topRight, (255, 0, 255), 2)
+            cv2.line(image, topRight, bottomRight, (255, 0, 255), 2)
+            cv2.line(image, bottomRight, bottomLeft, (255, 0, 255), 2)
+            cv2.line(image, bottomLeft, topLeft, (255, 0, 255), 2)
             
             cX = int((topLeft[0] + bottomRight[0]) / 2.0)
             cY = int((topLeft[1] + bottomRight[1]) / 2.0)
@@ -57,7 +57,7 @@ def aruco_display(corners, ids, rejected, image):
             outlineText = "ID: " + str(markerID) + " at " +  str(round(distance,2)) + " units"
 
             cv2.putText(image, outlineText,(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                0.5, (0, 255, 0), 2)
+                0.6, (255, 0, 255), 2)
             
     return image
 
